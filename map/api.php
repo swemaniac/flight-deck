d615cdcdfe8be2c3f75ffa95492d2e7d0662507a
swemaniac <swemaniac22@gmail.com>
2016-10-29 13:26:00 +0200

    Require db pass ext

--- map/api.php
+++ map/api.php
@@ -1,4 +1,6 @@
 <?php
+    require_once('db.inc');
+
     if ($_REQUEST['action'] == 'routes') {
         // select f.departure_icao as dep_icao, dep.name as dep_name, dep.lat as dep_lat, dep.lng as dep_lng, f.destination_icao as dest_icao, dest.name as dest_name, dest.lat as dest_lat, dest.lng as dest_lng from flights f inner join airports dep on dep.icao = f.departure_icao inner join airports dest on dest.icao = f.destination_icao where f.msn != '' and f.departure_icao != f.destination_icao group by f.departure_icao, f.destination_icao
 
@@ -19,6 +21,6 @@
     }
 
     function connect() {
-        return new PDO('mysql:host=localhost;dbname=flightdeck;charset=utf8mb4', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return new PDO('mysql:host=localhost;dbname=flight_deck;charset=utf8mb4', 'flight_deck', DB_PASS, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
 ?>
